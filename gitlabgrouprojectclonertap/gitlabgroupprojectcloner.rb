83949dca1610cd5ba2b92e714518e1e44367a5e7
nabin.shrestha <nabin.shrestha@f1soft.com>
2023-05-16 10:15:46 +0545

    WIP

--- gitlabgrouprojectclonertap/gitlabgroupprojectcloner.rb
+++ gitlabgrouprojectclonertap/gitlabgroupprojectcloner.rb
@@ -1,8 +1,8 @@
 class GitlabGroupProjectCloner < Formula
     desc "This package clones project from gitlab."
     homepage "https://gitlab.com/nabs107/gitlabprojectcloner.git"
-    url "https://github.com/your-username/your-project/archive/v1.0.0.tar.gz"
-    sha256 "..."  # The SHA-256 hash of the release tarball
+    url "https://gitlab.com/nabs107/gitlabprojectcloner/-/archive/1.0.0/gitlabprojectcloner-1.0.0.tar.gz"
+    sha256 "eb28774b04ca1022d439fdd0ba1ff6f228b7389bfbed"  # The SHA-256 hash of the release tarball
   
     depends_on "node"
   
@@ -12,7 +12,7 @@ class GitlabGroupProjectCloner < Formula
     end
   
     test do
-      system "#{bin}/your-command", "--version"
+      system "#{bin}/cloner", "--version"
     end
   end
   
